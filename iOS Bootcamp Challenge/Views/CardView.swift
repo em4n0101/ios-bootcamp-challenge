bb2db9b08249ca7715abeff78fec7197539bc368
Alejandro Barranco <alexbar10@live.com.mx>
2021-10-14 00:05:09 -0500

    Display pokemon info (eg. types, abilities)

--- iOS Bootcamp Challenge/Views/CardView.swift
+++ iOS Bootcamp Challenge/Views/CardView.swift
@@ -19,6 +19,16 @@ class CardView: UIView {
         label.translatesAutoresizingMaskIntoConstraints = false
         return label
     }()
+    
+    lazy private var itemsStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.distribution = .fillEqually
+        
+        stackView.spacing = margin/2
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        return stackView
+    }()
 
     required init(card: Card) {
         self.card = card
@@ -48,8 +58,30 @@ class CardView: UIView {
         titleLabel.topAnchor.constraint(equalTo: self.topAnchor, constant: margin * 2).isActive = true
         titleLabel.leftAnchor.constraint(equalTo: self.leftAnchor, constant: margin).isActive = true
         titleLabel.widthAnchor.constraint(lessThanOrEqualTo: self.widthAnchor, multiplier: 0.70).isActive = true
+        
+        addSubview(itemsStackView)
+        itemsStackView.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: margin).isActive = true
+        itemsStackView.leftAnchor.constraint(equalTo: self.leftAnchor, constant:  margin).isActive = true
+        itemsStackView.widthAnchor.constraint(lessThanOrEqualTo: self.widthAnchor, multiplier: 1.0).isActive = true
+        
+        // Display pokemon info (eg. types, abilities)
+        card?.items.forEach { item in
+            let itemTitleLabel = UILabel()
+            itemTitleLabel.textAlignment = .left
+            itemTitleLabel.translatesAutoresizingMaskIntoConstraints = false
+            itemTitleLabel.text = item.title
+            itemTitleLabel.font = UIFont.boldSystemFont(ofSize: 17)
+            itemsStackView.addArrangedSubview(itemTitleLabel)
 
-        // TODO: Display pokemon info (eg. types, abilities)
-    }
+            let itemDescriptionLabel = UILabel()
+            itemDescriptionLabel.textAlignment = .left
+            itemDescriptionLabel.translatesAutoresizingMaskIntoConstraints = false
+            itemDescriptionLabel.text = item.description.capitalized
+            itemDescriptionLabel.textColor = .darkGray
+            itemDescriptionLabel.font = UIFont.systemFont(ofSize: 17)
+            itemsStackView.addArrangedSubview(itemDescriptionLabel)
+
+        }
+     }
 
 }
